8fd7cbf97c841642ba294b05f1e2cbf821cf5589
Akki k <52394863+mr-akhilesh-kumawat@users.noreply.github.com>
2020-09-01 02:54:24 +0530

    Update getusers.js
    I added pagination

--- controller/getusers.js
+++ controller/getusers.js
@@ -2,6 +2,8 @@ const knex = require("../model/knex");
 const prettyfyUserDetails = require("../controller/logic").prettyfyUserDetails
 const city = require("./city");
 exports.alluser = async (userEmail ,req,res,next)=>{
+    let pageSize=Number(req.query.pageSize)
+    let pageNum = Number(req.query.pageNum)*Number(pageSize)-pageSize
     let ageMoreThan = Number(req.query.ageMoreThan) || 0
     let ageLessThan = Number(req.query.ageLessThan) || 999
     let cityId = req.query.cityId 
@@ -15,7 +17,7 @@ exports.alluser = async (userEmail ,req,res,next)=>{
             }else{
                 this.orWhereNotNull("cityId")
             }
-        })
+        }).limit(pageSize).offset(pageNum)
         .then(async(userdetail)=>{
             if(userdetail.length){
                 res.send({users:await prettyfyUserDetails(userdetail)})
